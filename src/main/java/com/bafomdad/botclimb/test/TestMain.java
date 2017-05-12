cb2da0323785509f9fabf196193a8e713a7088b3
bafomdad <cheesecake@operamail.com>
2017-05-11 19:15:38 -0700

    flerp

--- src/main/java/com/bafomdad/botclimb/test/TestMain.java
+++ src/main/java/com/bafomdad/botclimb/test/TestMain.java
@@ -1,11 +1,5 @@
 package com.bafomdad.botclimb.test;
 
-import com.bafomdad.botclimb.BotClimb;
-
-import javax.json.*;
-import java.io.InputStream;
-import java.net.URL;
-
 /**
  * Created by bafomdad on 5/1/2017.
  */
@@ -13,25 +7,6 @@ public class TestMain {
 
     public static void main(String[] args) {
 
-        String address = BotClimb.config.address;
-        String port = BotClimb.config.port;
-
-        try {
-            URL url = new URL("http://api.soldat.pl/v0/server/" + address + "/" + port + "/players");
-
-            InputStream is = url.openStream();
-            JsonReader rdr = Json.createReader(is);
-
-            JsonObject obj = rdr.readObject();
-            JsonArray results = obj.getJsonArray("Players");
-            if (results.isEmpty()) {
-                System.out.println("Empty!");
-                return;
-            }
-            System.out.println(results.toString());
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        System.out.println("You got this, Travis");
     }
 }
